ad66646bcf98d7a73cfd85245fbb91d6ffb42088
Dave Humphrey <dave@uesp.net>
2016-10-31 10:46:09 -0400

    update version

--- SpecialEsoBuildEditor.php
+++ SpecialEsoBuildEditor.php
@@ -33,10 +33,10 @@ class SpecialEsoBuildEditor extends SpecialPage
 		$out = $this->getOutput();
 		
 		$out->addHeadItem("uesp-esobuildeditor3-js", "<script src='http://esobuilds-static.uesp.net/resources/json2.js'></script>");
-		$out->addHeadItem("uesp-esobuildeditor1-css", "<link rel='stylesheet' href='http://esolog-static.uesp.net/resources/esoItemSearchPopup.css?version=27Oct2016' />");
-		$out->addHeadItem("uesp-esobuildeditor1-js", "<script src='http://esolog-static.uesp.net/resources/esoItemSearchPopup.js?version=27Oct2016'></script>");
-		$out->addHeadItem("uesp-esobuildeditor2-css", "<link rel='stylesheet' href='http://esobuilds-static.uesp.net/resources/esoEditBuild_embed.css?version=27Oct2016' />");
-		$out->addHeadItem("uesp-esobuildeditor2-js", "<script src='http://esobuilds-static.uesp.net/resources/esoEditBuild.js?version=27Oct2016'></script>");
+		$out->addHeadItem("uesp-esobuildeditor1-css", "<link rel='stylesheet' href='http://esolog-static.uesp.net/resources/esoItemSearchPopup.css?version=31Oct2016' />");
+		$out->addHeadItem("uesp-esobuildeditor1-js", "<script src='http://esolog-static.uesp.net/resources/esoItemSearchPopup.js?version=31Oct2016'></script>");
+		$out->addHeadItem("uesp-esobuildeditor2-css", "<link rel='stylesheet' href='http://esobuilds-static.uesp.net/resources/esoEditBuild_embed.css?version=31Oct2016' />");
+		$out->addHeadItem("uesp-esobuildeditor2-js", "<script src='http://esobuilds-static.uesp.net/resources/esoEditBuild.js?version=31Oct2016'></script>");
 		
 		/*
 		<link rel="stylesheet" href="http://esolog.uesp.net/resources/esocp_simple_embed.css" />
