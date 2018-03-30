f7e382f4944716942cc51eb1b2dfa2b8b229a2fa
Dave Humphrey <dave@uesp.net>
2018-03-29 20:09:22 -0400

    update and move to appropriate pages

--- SpecialEsoBuildData.php
+++ SpecialEsoBuildData.php
@@ -25,9 +25,14 @@ class SpecialEsoBuildData extends SpecialPage
 		$this->buildDataViewer->wikiContext = $this->getContext();
 		
 		//$this->getOutput()->addModules( 'ext.EsoBuildData' );
-		
+				
 		$request = $this->getRequest();
 		$output = $this->getOutput();
+				
+		$output->addHeadItem("uesp-esochardata-css", "<link rel='stylesheet' href='//esobuilds-static.uesp.net/resources/esobuilddata.css?version=29Mar2018' />");
+		$output->addHeadItem("uesp-tablesorter-js", "<script src='//esobuilds-static.uesp.net/resources/jquery.tablesorter.min.js'></script>");
+		$output->addHeadItem("uesp-esochardata-js", "<script src='//esobuilds-static.uesp.net/resources/esobuilddata.js?version=29Mar2018'></script>");
+		
 		$this->setHeaders();
 
 		$charId = $request->getText( 'id' );
