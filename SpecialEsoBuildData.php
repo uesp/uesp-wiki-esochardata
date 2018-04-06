582fcbd2508609d987c3b1ef6d25eb61fca0861d
Dave Humphrey <uespnet@gmail.com>
2018-04-06 10:23:26 -0400

    update version

--- SpecialEsoBuildData.php
+++ SpecialEsoBuildData.php
@@ -29,10 +29,10 @@ class SpecialEsoBuildData extends SpecialPage
 		$request = $this->getRequest();
 		$output = $this->getOutput();
 				
-		$output->addHeadItem("uesp-esochardata-css", "<link rel='stylesheet' href='//esobuilds-static.uesp.net/resources/esobuilddata.css?version=31Mar2018' />");
+		$output->addHeadItem("uesp-esochardata-css", "<link rel='stylesheet' href='//esobuilds-static.uesp.net/resources/esobuilddata.css?version=6Apr2018' />");
 		$output->addHeadItem("uesp-tablesorter-js", "<script src='//esobuilds-static.uesp.net/resources/jquery.tablesorter.min.js'></script>");
 		$output->addHeadItem("uesp-tablesorter-js", "<script src='//esobuilds-static.uesp.net/resources/jquery.visible.js'></script>");
-		$output->addHeadItem("uesp-esochardata-js", "<script src='//esobuilds-static.uesp.net/resources/esobuilddata.js?version=31Mar2018'></script>");
+		$output->addHeadItem("uesp-esochardata-js", "<script src='//esobuilds-static.uesp.net/resources/esobuilddata.js?version=6Apr2018'></script>");
 		
 		$this->setHeaders();
 
