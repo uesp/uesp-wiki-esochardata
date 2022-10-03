e085ea7a444a94cf3e0fdcaac6f096704ae72195
Dania Masoud <demasoud@uwaterloo.ca>
2022-10-03 09:42:39 -0400

    testing push/pull from Atom

--- SpecialEsoBuildData.php
+++ SpecialEsoBuildData.php
@@ -1,34 +1,34 @@
 <?php
 
+//testing push/pull from atom
 
-
-class SpecialEsoBuildData extends SpecialPage 
+class SpecialEsoBuildData extends SpecialPage
 {
 	public $buildDataViewer = null;
-	
-	
-	function __construct() 
+
+
+	function __construct()
 	{
 		global $wgOut;
-				
+
 		parent::__construct( 'EsoBuildData' );
-		
+
 		$wgOut->addModules( 'ext.EsoBuildData.viewer.scripts' );
 		$wgOut->addModuleStyles( 'ext.EsoBuildData.viewer.styles' );
-		
+
 		$this->buildDataViewer = new EsoBuildDataViewer();
 		$this->buildDataViewer->baseUrl = "/wiki/Special:EsoBuildData";
 		$this->buildDataViewer->baseResourceUrl = "/esobuilddata/";
 	}
-	
 
-	function execute( $par ) 
+
+	function execute( $par )
 	{
 		$this->buildDataViewer->wikiContext = $this->getContext();
-		
+
 		$request = $this->getRequest();
 		$output = $this->getOutput();
-		
+
 		$this->setHeaders();
 
 		$charId = $request->getText( 'id' );
@@ -36,11 +36,11 @@ class SpecialEsoBuildData extends SpecialPage
 
 		$output->addHTML($this->buildDataViewer->getOutput());
 	}
-	
-	
-	function getGroupName() 
+
+
+	function getGroupName()
 	{
 		return 'wiki';
 	}
-	
+
 };
